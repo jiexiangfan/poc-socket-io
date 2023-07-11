d2de9529c5f257a7ac73fa0c3e75797edcd9c5a9
Jie Fan <jxiangfan@gmail.com>
2023-07-12 09:08:34 +1000

    Keep only server

--- controller/src/components/ConnectionState.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function ConnectionState({ isConnected }) {
-  return (
-    <>
-      <p style={{ fontSize: "8px" }}>Connection: {"" + isConnected}</p>
-    </>
-  );
-}
--- server/server.js
+++ server/server.js
@@ -8,17 +8,6 @@ const io = new Server(3000, {
 io.on("connection", (socket) => {
   console.log("Socket.io connected to socket: " + socket.id);
 
-  // socket.on("create-event", (value) => {
-  //   console.log("Received foo event: " + value);
-  //   io.emit("foo", value);
-  // });
-
-  // socket.on("from-controller", (value) => {
-  //   console.log("Received from controller: " + value);
-  //   io.emit("to-monitor", value);
-  //   console.log("Broadcasted the signal above to monitor");
-  // });
-
   socket.on("send-disp-list", (list) => {
     console.log("Received from controller: " + list);
     io.emit("receive-disp-list", list);
